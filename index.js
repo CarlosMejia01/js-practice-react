36535162677f03a66f205d7fd7ffd1460027c3b4
cmejia <cmejia@equitel.com.co>
2023-01-24 00:56:07 -0500

    feat: New object and print data

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const user = {
-  x: 20,
+  name: "ryan",
+  lastname: "perez",
+  age: 30,
+  address: {
+    country: "Colombia",
+    city: "Bogota",
+    street: "main street 123",
+  },
+  friends: ["brandon", "elena"],
+  active: true,
+  sendMail: function () {
+    return "Sending Email...";
+  },
 };
+
+console.log(user);
