7faa21be03864b9252147b0ca820b3c66cc516f9
cmejia <cmejia@equitel.com.co>
2023-02-01 13:30:53 -0500

    feat: export modules in js

--- add.js
+++ add.js
@@ -1,3 +1,13 @@
 export function add(x, y) {
   return x + y;
 }
+
+export function multiply(x, y) {
+  return x * y;
+}
+
+export const title = "js for react";
+export const active = true;
+export const points = [10, 20, 30];
+
+export default title;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { add } from "./add.js";
+import { add, multiply, active, points, title } from "./add.js";
 
+console.log(active, points, title);
 console.log(add(10, 5));
+console.log(multiply(10, 5));
