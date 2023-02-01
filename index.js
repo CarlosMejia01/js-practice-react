dca43c45f3b607c16543e49c5ef260d87c73be4f
cmejia <cmejia@equitel.com.co>
2023-02-01 13:56:14 -0500

    feat: async await

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const ul = document.createElement("ul");
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    data.forEach(function (post) {
-      const li = document.createElement("li");
-      li.innerText = post.title;
-      ul.append(li);
-      document.body.append(ul);
-    });
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     data.forEach(function (post) {
+//       const li = document.createElement("li");
+//       li.innerText = post.title;
+//       ul.append(li);
+//       document.body.append(ul);
+//     });
+//   });
+
+async function loadData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  data.forEach(function (post) {
+    const li = document.createElement("li");
+    li.innerText = post.title;
+    ul.append(li);
+    document.body.append(ul);
   });
+}
+
+loadData();
 
 console.log("linea 2");
