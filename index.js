be2bbb722c0e521e3ad8ba693601b6110264f48b
cmejia <cmejia@equitel.com.co>
2023-02-01 12:23:00 -0500

    feat: return

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const showText = () => "Hola Mundo";
-const showNumber = () => 22;
-const showBoolean = () => true;
-const showArray = () => [1, 2, 3];
-const showObject = () => ({ name: "Charly" });
-
-console.log(showText());
-console.log(showNumber());
-console.log(showBoolean());
-console.log(showArray());
-console.log(showObject());
-
 const button = document.createElement("button");
 button.innerText = "click me";
 
+const isAuthorized = true;
+
 button.addEventListener("click", () => {
-  alert("click");
+  if (isAuthorized) return alert("esta autorizado");
+  alert("no esta autorizado");
 });
 
 document.body.append(button);
