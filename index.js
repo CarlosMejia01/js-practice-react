40f3cc1a8a5d213b6cc1f256ea2e59664e33247f
cmejia <cmejia@equitel.com.co>
2023-02-01 13:49:52 -0500

    feat: fetch get data api jsonplaceholder

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-fetch("https://jsonplaceholder.typicode.com/posts").then(function () {
-  console.log("finalizo la carga");
-});
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 console.log("linea 2");
