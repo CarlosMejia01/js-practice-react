eb74346967a3215fad7cba7471a16b13f14a3a70
cmejia <cmejia@equitel.com.co>
2023-02-01 11:22:43 -0500

    feat: Change text on click event listener

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ title.innerText = "Hola mundo desde JS";
 const button = document.createElement("button");
 button.innerText = "click";
 
+button.addEventListener("click", function () {
+  title.innerText = "Texto actualizado con JS";
+});
+
 document.body.append(title);
 document.body.append(button);
