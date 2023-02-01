6201d5781deba602b0e8d21593ddb0a5f7e74c4e
cmejia <cmejia@equitel.com.co>
2023-02-01 12:28:40 -0500

    feat: string literals

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const background = "red";
+const color = "white";
+const isAuthorized = true;
+
 const button = document.createElement("button");
 button.innerText = "click me";
-
-const isAuthorized = true;
+button.style = `background: ${
+  isAuthorized ? "blue" : background
+}; color: ${color}`;
 
 button.addEventListener("click", () => {
   if (isAuthorized) return alert("esta autorizado");
