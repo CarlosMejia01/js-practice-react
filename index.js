db839dcd03f52cea23e0f57e6c4163d09ff14eb9
cmejia <cmejia@equitel.com.co>
2023-02-01 11:40:44 -0500

    feat: Anonymous function

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const user = {
-  name: "Joe",
-  age: 30,
-};
-
-function printInfo(user) {
-  let { name, age } = user;
-  console.log(name, age);
-  return "<h1>Hola " + name + "</h1>";
-}
-
-console.log(printInfo(user));
-
-document.body.innerHTML = printInfo(user);
+console.log(
+  (function () {
+    return "Starting...";
+  })()
+);
