e45c42adfe60d46274c655c7bbb39192774ccaf1
cmejia <cmejia@equitel.com.co>
2023-02-01 13:52:53 -0500

    feat: append data to ul

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const ul = document.createElement("ul");
+
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    data.forEach(function (post) {
+      const li = document.createElement("li");
+      li.innerText = post.title;
+      ul.append(li);
+      document.body.append(ul);
+    });
   });
 
 console.log("linea 2");
