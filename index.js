ba44403cacccdc3130e7b51d3c5cc741c10ebc5a
cmejia <cmejia@equitel.com.co>
2023-02-01 12:06:01 -0500

    feat: inline arrow function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const showText = () => "Hola Mundo";
+const showNumber = () => 22;
+const showBoolean = () => true;
+const showArray = () => [1, 2, 3];
+const showObject = () => ({ name: "Charly" });
+
+console.log(showText());
+console.log(showNumber());
+console.log(showBoolean());
+console.log(showArray());
+console.log(showObject());
+
 const button = document.createElement("button");
 button.innerText = "click me";
 
