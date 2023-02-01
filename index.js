e3b32aca7a63316afeabce4fe7f1b4d6f1643a39
cmejia <cmejia@equitel.com.co>
2023-02-01 11:58:44 -0500

    feat: anonymous function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log(
-  (function () {
-    return "Starting...";
-  })()
-);
+const button = document.createElement("button");
+button.innerText = "click me";
+
+button.addEventListener("click", function () {
+  alert("click");
+});
+
+document.body.append(button);
